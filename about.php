edb05e21e1df70918a62f3c943178fecc8e77bba
ij9m <ma0214@mix.wvu.edu>
2020-09-27 22:10:49 -0400

    note

---
+++ about.php
@@ -0,0 +1,95 @@
+<?php 
+require 'includes/header.php'
+?>
+
+<main>
+<link rel="stylesheet" href="../css/about.css">
+    <div class="about">
+        <h1>About Our Team</h1>
+        <p>Our team is concerned about two main topics: Quantum Phyiscs and Relativity</p>
+    </div>
+
+
+    <div class="row">
+        <div class="column">
+            <div class="member">
+                <img src="images/einstein.jpg">
+                <div class="container">
+                    <h2>
+                        Albert Einstein
+                    </h2>
+                    <p>
+                        A German-born theoretical physicist who developed the theory of relativity, one of the two
+                        pillars of modern physics alongside quantum mechanics, his work is also known for its influence
+                        on the philosophy of science.
+                    </p>
+            
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="member">
+                <img src="images/bohr.jpg">
+                <div class="container">
+                    <h2>
+                        Niels Bohr
+                    </h2>
+                    <p>
+                        A Danish physicist who made foundational contributions to understanding atomic structure and
+                        quantum theory, for which he received the Nobel Prize in Physics in 1922. Bohr was also a
+                        philosopher and a promoter of scientific research.
+
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="column">
+            <div class="member">
+                <img src="images/heisenberg.jpg">
+                <div class="container">
+                    <h2>
+                        Warner Heisenberg
+                    </h2>
+                    <p>
+                        A German theoretical physicist and one of the key pioneers of quantum mechanics. He published
+                        his work in 1925 in a breakthrough paper. In the subsequent series of papers with Max Born and
+                        Pascual Jordan, during the same year, this matrix formulation
+                        of quantum mechanics was substantially elaborated.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="member">
+                <img src="images/schrod.jpg">
+                <div class="container">
+                    <h2>
+                        Erwin Schrödinger
+                    </h2>
+                    <p>
+                        A Nobel Prize-winning Austrian-Irish physicist who developed a number of fundamental results in
+                        quantum theory: the Schrödinger equation provides a way to calculate the wave function of a
+                        system and how it changes dynamically in time.
+                    </p>
+                </div>
+            </div>*/
+
+        </div>
+
+
+
+    </div>
+
+    <div class="button">
+        <div>
+            <button type="button1">Back</button>
+            <button type="button2">Next</button>
+        </div>
+
+    </div>
+</main>
+
